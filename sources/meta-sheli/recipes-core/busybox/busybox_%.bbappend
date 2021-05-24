c8363898e12ee990d9cb2c240bd49b895f5c7243
bielu000 <bielu000@o2.pl>
2021-05-24 21:23:46 +0200

    Added fragment file to busybox which adds custom busybox cfg. Added syslog logging in daemon

---
+++ sources/meta-sheli/recipes-core/busybox/busybox_%.bbappend
@@ -0,0 +1,4 @@
+
+SRC_URI += "file://fragment.cfg"
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
--- sources/meta-sheli/recipes-examples/my-service/files/skeleton_test.c
+++ sources/meta-sheli/recipes-examples/my-service/files/skeleton_test.c
@@ -1,18 +1,17 @@
 #include <unistd.h>
+#include <syslog.h>
 
-
-volatile int* ptr;
-int xyz = 10;
+#define DAEMON_NAME "simpledaemon"
 
 int main(int argc, char *argv[])
 {
-	daemon(0, 0);
-	while (1) {
-		if (ptr)
-		{
-			xyz = 99;
-		}
+	setlogmask(LOG_UPTO(LOG_INFO));
+	openlog(DAEMON_NAME, LOG_CONS | LOG_PERROR, LOG_USER);
 
+	daemon(0, 0);
+
+	while (1) {
+		syslog(LOG_INFO, "daemon running");
 		sleep(30);
 	}
 	return 0;
