3fed15ea451a8f350c5f680cda70fe0fa2fac410
bielu000 <bielu000@o2.pl>
2021-05-24 19:16:14 +0200

    Added sysvinit example service.

--- sources/meta-sheli/recipes-examples/my-service/files/skeleton_test.c
+++ sources/meta-sheli/recipes-examples/my-service/files/skeleton_test.c
@@ -2,10 +2,17 @@
 
 /* This demo does nothing except for testing /etc/init.d/skeleton */
 
+volatile int* ptr;
+int xyz = 10;
+
 int main(int argc, char *argv[])
 {
 	daemon(0, 0);
 	while (1) {
+		if (ptr)
+		{
+			xyz = 99;
+		}
 		sleep(1);
 	}
 	return 0;
